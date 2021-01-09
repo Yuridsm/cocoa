664ce4093b1209a3e6da4c652f11bfecb6d87bc8
Yuri <yurifullstack@gmail.com>
2021-01-09 14:31:23 -0300

    Home Controller

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -15,6 +15,7 @@ namespace cocoa.Controllers
     public class HomeController : ControllerBase
     {
         public static int Count { get; set; }
+        private readonly IDateTime _datetime;
 
         public readonly IRenderSingleton Singleton0;
         public readonly IRenderSingleton Singleton1;
@@ -31,7 +32,8 @@ namespace cocoa.Controllers
             IRenderSingleton singleton0,
             IRenderSingleton singleton1,
             IRenderTransient transient0,
-            IRenderTransient transient1
+            IRenderTransient transient1,
+            IDateTime datetime
         )
         {
             Scope0 = scope0;
@@ -40,12 +42,13 @@ namespace cocoa.Controllers
             Singleton1 = singleton1;
             Transient0 = transient0;
             Transient1 = transient1;
+            _datetime = datetime;
         }
 
         [HttpGet]
         public IActionResult Index()
         {
-            return Ok(Scope0.GetGuidNow.ToString());
+            return Ok(_datetime.Now);
         }
 
     }
