6dee0afb050ba5f73dedc851ab9d186716cc3d57
Yuri <yurifullstack@gmail.com>
2021-01-09 14:31:55 -0300

    Setting up IPersonalInfo and PersonalInfoService

--- Startup.cs
+++ Startup.cs
@@ -31,6 +31,10 @@ namespace cocoa
             services.AddTransient<IRenderTransient, RenderTransientService>();
 
             services.AddSingleton<IHealth, HealthService>();
+
+            services.AddSingleton<IDateTime, SystemDatetime>();
+
+            services.AddScoped<IPersonalInfo, PersonalInfoService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
