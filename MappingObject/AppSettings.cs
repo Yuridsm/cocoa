ff74f2c335883fcd28ff6c1b0e083de3aa6972da
Yuri <yurifullstack@gmail.com>
2021-01-09 16:35:31 -0300

    Mapping Objects

--- MappingObject/AppSettings.cs
+++ MappingObject/AppSettings.cs
@@ -9,21 +9,7 @@ namespace cocoa.MappingObject
 {
     public class AppSettings : IAppSettings
     {
-        public string Logging => GetRequiredAppSetting("Logging");
-        public string LogLevel => GetRequiredAppSetting("LogLevel");
-        public string Default => GetRequiredAppSetting("Default");
-        public string Microsoft => GetRequiredAppSetting("Microsoft");
-        public string Microsoft_Hosting_Lifetime => GetRequiredAppSetting("Microsoft.Hosting.Lifetime");
-
-        private static string GetRequiredAppSetting(string key)
-        {
-            var value = ConfigurationManager.AppSettings[key];
-
-            if (value == null)
-                throw new Exception();
-
-            return value;
-        }
-
+        public string Logging { get; set; }
+        public LogLevel LogLevel { get; set; }
     }
 }
