1134d245289a8a3cc652b555b8fd5dd4933e7998
Yuri <yurifullstack@gmail.com>
2021-01-09 16:40:54 -0300

    LogLevel

---
+++ MappingObject/LogLevel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace cocoa.MappingObject
+{
+    public class LogLevel
+    {
+        public string Default { get; set; }
+        public string Microsoft { get; set; }
+        public string Microsoft_Hosting_Lifetime { get; set; }
+    }
+}
