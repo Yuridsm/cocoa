e770104562c1a72b2f1166d762cbec52506139d2
Yuri <yurifullstack@gmail.com>
2021-01-08 23:59:00 -0300

    RenderScoped Service

---
+++ Services/RenderScopedService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using cocoa.Interfaces;
+
+namespace cocoa.Services
+{
+    public class RenderScopedService : IRenderScoped
+    {
+        public Guid GetGuidNow { get; private set; } = Guid.NewGuid();
+    }
+}
