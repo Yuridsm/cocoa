dacc3350d02fa5217c2253bdabdace5c454f4666
Yuri <yurifullstack@gmail.com>
2021-01-09 16:34:23 -0300

    IAppSettings interface

--- Interfaces/IAppSettings.cs
+++ Interfaces/IAppSettings.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using cocoa.MappingObject;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -8,9 +9,6 @@ namespace cocoa.Interfaces
     public interface IAppSettings
     {
         public string Logging { get; }
-        public string LogLevel { get; }
-        public string Default { get; }
-        public string Microsoft { get; }
-        public string Microsoft_Hosting_Lifetime { get; }
+        public LogLevel LogLevel { get; }
     }
 }
