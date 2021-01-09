ad2526e421a508e037792fc4fed52786ee6eb1ec
Yuri <yurifullstack@gmail.com>
2021-01-09 14:33:25 -0300

    Interface for my personal informations

---
+++ Interfaces/IPersonalInfo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace cocoa.Interfaces
+{
+    public interface IPersonalInfo
+    {
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public int age { get; set; }
+        public string description { get; set; }
+    }
+}
