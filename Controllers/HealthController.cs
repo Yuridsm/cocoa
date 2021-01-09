5e5e286406776f21e9dc43b8804c650a139b0dd7
Yuri <yurifullstack@gmail.com>
2021-01-08 23:58:30 -0300

    Health Controller

---
+++ Controllers/HealthController.cs
@@ -0,0 +1,21 @@
+﻿using cocoa.Interfaces;
+using cocoa.MappingObject;
+using cocoa.Providers;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using Newtonsoft.Json;
+
+namespace cocoa.Controllers
+{
+    [Route("[controller]")]
+    public class HealthController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            var r = HealthProvider.GetSettings();
+            var serializerSettings = new JsonSerializerSettings { Formatting = Formatting.Indented };
+            return Json(r, serializerSettings);
+        }
+    }
+}
