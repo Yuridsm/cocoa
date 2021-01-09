cd1e3631e6967259bee50041b117d33b76349539
Yuri <yurifullstack@gmail.com>
2021-01-08 23:58:06 -0300

    Testing the things

--- Program.cs
+++ Program.cs
@@ -6,6 +6,10 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using cocoa.Interfaces;
+using cocoa.Services;
+using cocoa.Controllers;
 
 namespace cocoa
 {
