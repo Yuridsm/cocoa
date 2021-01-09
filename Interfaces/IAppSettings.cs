09910ea1ff2f84c6f77c75921361cad012df7ea5
Yuri <yurifullstack@gmail.com>
2021-01-08 23:58:36 -0300

    My Interfaces

---
+++ Interfaces/IAppSettings.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace cocoa.Interfaces
+{
+    public interface IAppSettings
+    {
+        public string Logging { get; }
+        public string LogLevel { get; }
+        public string Default { get; }
+        public string Microsoft { get; }
+        public string Microsoft_Hosting_Lifetime { get; }
+    }
+}
---
+++ Interfaces/IHealth.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace cocoa.Interfaces
+{
+    public interface IHealth
+    {
+        public string RAMMemoryInGB { get; set; }
+        public string OperatingSystem { get; set; }
+        public int Cores { get; set; }
+
+        public string GetInfo();
+    }
+}
