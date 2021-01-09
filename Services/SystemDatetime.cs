cb26f9a828ec086f3b0d969ca788b6df4004b261
Yuri <yurifullstack@gmail.com>
2021-01-09 14:35:02 -0300

    SystemDatetime service

---
+++ Services/SystemDatetime.cs
@@ -0,0 +1,16 @@
+﻿using cocoa.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace cocoa.MappingObject
+{
+    public class SystemDatetime : IDateTime
+    {
+        public DateTime Now
+        {
+            get { return DateTime.Now; }
+        }
+    }
+}
