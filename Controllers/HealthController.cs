97a2ed002d91e593d666420509c76a4522291027
Yuri <yurifullstack@gmail.com>
2021-01-09 16:32:50 -0300

    Health Controller

--- Controllers/HealthController.cs
+++ Controllers/HealthController.cs
@@ -10,12 +10,20 @@ namespace cocoa.Controllers
     [Route("[controller]")]
     public class HealthController : ControllerBase
     {
+        private readonly AppSettings _appSettings;
+
+        public HealthController(IOptions<AppSettings> settingsOpt)
+        {
+            _appSettings = settingsOpt.Value;
+        }
+
+
         [HttpGet]
         public IActionResult Index()
         {
             var r = HealthProvider.GetSettings();
             var serializerSettings = new JsonSerializerSettings { Formatting = Formatting.Indented };
-            return Ok(r);
+            return Ok(_appSettings);
         }
     }
 }
