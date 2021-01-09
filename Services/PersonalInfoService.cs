b39874c568ba0cdafdb18497ffd58ab351366b2f
Yuri <yurifullstack@gmail.com>
2021-01-09 14:34:50 -0300

    PersonalInfo Service

---
+++ Services/PersonalInfoService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using cocoa.Interfaces;
+
+namespace cocoa.Services
+{
+    public class PersonalInfoService : IPersonalInfo
+    {
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public int age { get; set; }
+        public string description { get; set; }
+    }
+}
