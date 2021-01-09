ace37b8276b2c5d2a34f56c44e3b4778ae450140
Yuri <yurifullstack@gmail.com>
2021-01-09 16:36:21 -0300

    Program

--- Program.cs
+++ Program.cs
@@ -22,6 +22,12 @@ namespace cocoa
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+                .ConfigureAppConfiguration((hostingContext, config) =>
+                {
+                    config.AddJsonFile("appsettings.json",
+                        optional: false,
+                        reloadOnChange: true);
+                })
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
