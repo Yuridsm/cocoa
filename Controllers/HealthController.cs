73c48de0881827cd0eba1395d290ec916fd759c4
Yuri <yurifullstack@gmail.com>
2021-01-10 15:14:02 -0300

    Health Controller

--- Controllers/HealthController.cs
+++ Controllers/HealthController.cs
@@ -3,25 +3,26 @@ using cocoa.MappingObject;
 using cocoa.Providers;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Options;
-using Newtonsoft.Json;
+using System;
+using System.IO;
+using System.Text.Json;
 
 namespace cocoa.Controllers
 {
     [Route("[controller]")]
     public class HealthController : ControllerBase
     {
-        private readonly AppSettings _appSettings;
-
-        public HealthController(IOptions<AppSettings> settingsOpt)
+        public AppSettings _appSettings { get; }
+        public HealthController(IOptions<AppSettings> appSettings)
         {
-            _appSettings = settingsOpt.Value;
+            this._appSettings = appSettings.Value;
         }
 
         [HttpGet]
         public IActionResult Index()
         {
-            var r = HealthProvider.GetSettings();
-            var serializerSettings = new JsonSerializerSettings { Formatting = Formatting.Indented };
+            //var r = HealthProvider.GetSettings(this);
+            var serializerSettings = JsonSerializer.Deserialize<AppSettings>(_appSettings)
             return Ok(_appSettings);
         }
     }
